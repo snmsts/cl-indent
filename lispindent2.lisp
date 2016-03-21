871c327c593207b433375ced64a2ff2ddd130e41
SANO Masatoshi <snmsts@gmail.com>
2016-03-21 19:09:42 +0900

    reading...

--- lispindent2.lisp
+++ lispindent2.lisp
@@ -4,26 +4,23 @@
 #+yasi-as-library
 (in-package :cl-indent)
 
-(defvar *lisp-keywords* '())
+(defvar *lisp-keywords* (or #+yasi-as-library'() '()))
 (defvar *tab-size* 8)
 (defvar *mode* '())
+
 #-yasi-as-library
-(progn
-  (defvar *file-name* (first *args*))
-  (defvar *output-to-stdout* (not (member "--no-output" *args* :test #'string-equal)))
-  (defvar *modify-file* (not (member "--no-modify" *args* :test #'string-equal)))
-  (unless *args* ;; print help if no command-line arguments are passed to the file
-    (princ "
+(unless *args* ;; print help if no command-line arguments are passed to the file
+  (princ "
  ___________________________________________________________________________
 |   Usage:  lispindent2.0.lisp [[<file>] [--no-modify] [--no-output]]       |
 |           --no-output ;; Don't output the indented code, false by default |
 |           --no-modify ;; Don't modify the file, false by default          |
 +---------------------------------------------------------------------------+
 ")
-    (exit)))
+  (exit))
 
 (defun define-with-lisp-indent-number (n syms)
-  " Associates the keywords in the supplied list with the number in the first
+  "Associates the keywords in the supplied list with the number in the first
   argument and stores them in the global *lisp-keywords* variable for later
   reference e.g (('LOOP' . 0) ('HANDLER-BIND' . 0) ('BLOCK' . 0)) "
   (dolist (sym syms)
@@ -59,17 +56,6 @@
     multiple-value-bind
     with-slots))
 
-;; Get lisp keywords stored in your home directory and add them to the *lisp-words*
-;; list.
-
-#-yasi-as-library
-(with-open-file (i (merge-pathnames ".lispwords" (user-homedir-pathname))
-                   :if-does-not-exist nil)
-  (when i
-    (loop
-      (let ((w (or (read i nil) (return))))
-        (define-with-lisp-indent-number (car w) (cdr w))))))
-
 #|
   Basically returns the position of the end of this token(symbol,variable e.t.c), e.g:
     (past-next-token " this that " 0 8)      => 0
@@ -85,36 +71,35 @@
 |#
 
 (defun past-next-token (str i n)
-  (let ((escapep nil))
-    (loop
-      (when (>= i n) (return i))
-      (let ((c (char str i))) ;; c holds the current character.
-        (cond (escapep (setq escapep nil)) ;; if true set to false
-              ((char= c #\\) (setq escapep t))
-              ((char= c #\#)
-               (let ((j (+ i 1)))
-                 (if (>= j n) (return i)
-                   (let ((c (char str j)))
-                     (cond ((char= c #\\) (setq escapep t i j)) ;; found character literal, stop.
-                           (t (return i)))))))
-              ((member c '(#\space #\tab #\( #\) #\[ #\] #\" #\' #\` #\, #\; #\} #\{))
-             ;  (format t "token: `~a`" c)
-               (return i))))
-      (incf i))))
+  (loop
+     with escapep = nil
+     for c = (char str i) ;; c holds the current character.
+     do (when (>= i n) (return i))
+       (cond (escapep (setq escapep nil)) ;; if true set to false
+             ((char= c #\\) (setq escapep t))
+             ((char= c #\#)
+              (let ((j (+ i 1)))
+                (if (>= j n) (return i)
+                    (let ((c (char str j)))
+                      (cond ((char= c #\\) (setq escapep t i j)) ;; found character literal, stop.
+                            (t (return i)))))))
+             ((member c '(#\Space #\Tab #\( #\) #\[ #\] #\" #\' #\` #\, #\; #\} #\{))
+              ;; (format t "token: `~a`" c)
+              (return i)))
+       (incf i)))
 
 (defun lisp-indent-number (str &optional (possible-keyword-p t))
   "Returns the indentation number for the keyword if it is *lisp-keywords*. If it
  starts with 'def', it's indent value is 0. if it has a colon preceding it, the
  rest of the string is tested recursively to see whether it is a keyword."
-  (or (cdr (assoc str *lisp-keywords* :test #'string-equal))
-      (if (zerop (or (search "def" str :test #'char-equal) -1))
-          0
-        (if possible-keyword-p
-            (let ((colon-pos (position #\: str :from-end t)))
-              (if colon-pos
-                  (lisp-indent-number (subseq str (1+ colon-pos)) nil)
-                -1))
-          -1))))
+  (cond ((cdr (assoc str *lisp-keywords* :test #'string-equal)))
+        ((zerop (or (search "def" str :test #'char-equal) -1)) 0)
+        (possible-keyword-p
+         (let ((colon-pos (position #\: str :from-end t)))
+           (if colon-pos
+               (lisp-indent-number (subseq str (1+ colon-pos)) nil)
+               -1)))
+        (t -1)))
 
 (defun literal-token-p (str)
   (let ((colon-pos (position #\: str)))
@@ -147,6 +132,7 @@
  (calc-subindent "'(   (condition that)" 0 20) ;; ==> 1
  (calc-subindent ":define   (condition that)" 0 20) ;; ==> 2
 |#
+
 (defun calc-subindent (str i n)
   (let* ((j (past-next-token str i n)) ;; store position of start of the next token
          (num-aligned-subforms 0)
@@ -172,16 +158,16 @@
   (let ((n (length str))
         (i 0))
     (loop
-      (when (>= i n)
-        (return 0))
-      (case (char str i)
-        (#\Space (incf i))
-        (#\Tab (incf i *tab-size*))
-        (t (return i))))))
+       (when (>= i n)
+         (return 0))
+       (case (char str i)
+         (#\Space (incf i))
+         (#\Tab (incf i *tab-size*))
+         (t (return i))))))
 
 (defun string-trim-blanks (s)
   "Remove leading and trailing whitespace even in a string."
-  (string-trim '(#\space #\tab #\newline #\return) s))
+  (string-trim '(#\Space #\Tab #\Newline #\Return) s))
 
 (defun indent-lines (&optional (file-with-code *standard-input*) (indented-file *standard-output*))
   (loop
@@ -211,9 +197,9 @@
      do
        (setq curr-line (string-trim-blanks curr-line)) ;; remove leading to be added according to the indentation level
        (dotimes (k curr-left-i)
-         (write-char #\space indented-file)) ;; print leading spaces corresponding to the indent level.
+         (write-char #\Space indented-file)) ;; print leading spaces corresponding to the indent level.
        (princ curr-line indented-file)
-       (write-char #\linefeed indented-file) ;; print the line with the correct indentation and a newline(terpri)
+       (write-char #\Linefeed indented-file) ;; print the line with the correct indentation and a newline(terpri)
        (loop ;; walk the string character by character
           with str-len = (length curr-line)
           with escapep
@@ -227,7 +213,7 @@
                    ((char= c #\;) (return)) ;; found a comment, skip the rest of the line
                    ((char= c #\") (setq stringp t)) ;; switch the string predicate to true, found a string or comment.
                    ((char= c #\|) (setq multiline-commentp t)) ;; switch the multiline predicate to true, found a multiline comment.
-                   ((member c '(#\space #\tab) :test #'char=)
+                   ((member c '(#\Space #\Tab) :test #'char=)
                     (unless inter-word-space-p ;; inter-word-space-p helps us ignore consecutive spaces that would give a false detection of an if-clause.
                       (setq inter-word-space-p t)
                       (let ((blk (car block-stack)))
@@ -253,6 +239,16 @@
 
 #-yasi-as-library
 (progn
+  (defvar *file-name* (first *args*))
+  (defvar *output-to-stdout* (not (member "--no-output" *args* :test #'string-equal)))
+  (defvar *modify-file* (not (member "--no-modify" *args* :test #'string-equal)))
+  ;; Get lisp keywords stored in your home directory and add them to the *lisp-words* list.
+  (with-open-file (i (merge-pathnames ".lispwords" (user-homedir-pathname))
+                     :if-does-not-exist nil)
+    (when i
+      (loop
+         (let ((w (or (read i nil) (return))))
+           (define-with-lisp-indent-number (car w) (cdr w))))))
   (and (search ".clj" *file-name*)
        (= 4 (- (length *file-name*)
                (search ".clj" *file-name* :from-end t)))
